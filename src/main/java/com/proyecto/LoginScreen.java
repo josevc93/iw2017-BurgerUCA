2b7931262758642cfc6858d61e5729fb319688fa
DavidBlancoSD <david.blancocanizares@alum.uca.es>
2017-05-18 17:56:51 +0200

    LoginScreen

--- src/main/java/com/proyecto/LoginScreen.java
+++ src/main/java/com/proyecto/LoginScreen.java
@@ -1,35 +1,115 @@
 package com.proyecto;
 
 import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.server.Page;
+import com.vaadin.server.Responsive;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 public class LoginScreen extends VerticalLayout {
 
 	  
 	public LoginScreen(LoginCallback callback) {
-        setMargin(true);
-        setSpacing(true);
+		
+		setSizeFull();
+		
+		Component loginForm = buildLoginForm(callback);
+		addComponent(loginForm);
+		setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
+		
+		Notification notification = new Notification("Bienvenido a BurguerUCA!");
+	    //notification.setDescription("<span>This application is not real, it only demonstrates an application built with the <a href=\"https://vaadin.com\">Vaadin framework</a>.</span> <span>No username or password is required, just click the <b>Sign In</b> button to continue.</span>");
+	    notification.setHtmlContentAllowed(true);
+	    notification.setStyleName("Ayuda");
+	    notification.setPosition(Position.BOTTOM_CENTER);
+	    notification.setDelayMsec(20000);
+	    notification.show(Page.getCurrent());
+	}
+	
+	private Component buildLoginForm(LoginCallback callback) {
+		final VerticalLayout loginPanel = new VerticalLayout();
+        loginPanel.setSizeUndefined();
+        loginPanel.setSpacing(true);
+        Responsive.makeResponsive(loginPanel);
+        loginPanel.addStyleName("login-panel");
 
-        TextField username = new TextField("Username");
-        addComponent(username);
+        loginPanel.addComponent(buildLabels());
+        loginPanel.addComponent(buildFields(callback));
+        loginPanel.addComponent(new CheckBox("¿Has olvidado tu contraseña?", true));
+        return loginPanel;
+	}
+	
+	private Component buildFields(LoginCallback callback) {
+        HorizontalLayout fields = new HorizontalLayout();
+        fields.setSpacing(true);
+        fields.addStyleName("fields");
 
-        PasswordField password = new PasswordField("Password");
-        addComponent(password);
+        final TextField username = new TextField("Nombre");
+        //username.setIcon(FontAwesome.USER);
+        username.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
 
-        Button login = new Button("Login", evt -> {
+        final PasswordField password = new PasswordField("Contraseña");
+        //password.setIcon(FontAwesome.LOCK);
+        password.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+        Button login = new Button("Iniciar sesión", evt -> {
             String pword = password.getValue();
             password.setValue("");
             if (!callback.login(username.getValue(), pword)) {
-                Notification.show("Login failed");
+                Notification.show("Usuario o contraseña incorrectos!");
                 username.focus();
             }
         });
         login.setClickShortcut(ShortcutAction.KeyCode.ENTER);
         addComponent(login);
+        
+
+        //final Button signin = new Button("Iniciar sesión");
+        login.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        login.setClickShortcut(KeyCode.ENTER);
+        login.focus();
+
+        fields.addComponents(username, password, login);
+        fields.setComponentAlignment(login, Alignment.BOTTOM_LEFT);
+        /*
+        signin.addClickListener(new ClickListener() {
+            @Override
+            public void buttonClick(final ClickEvent event) {
+                DashboardEventBus.post(new UserLoginRequestedEvent(username.getValue(), password.getValue()));
+            }
+        });*/
+        return fields;
+    }
+	
+	private Component buildLabels() {
+        CssLayout labels = new CssLayout();
+        labels.addStyleName("labels");
+
+        Label welcome = new Label("BurguerUCA");
+        welcome.setSizeUndefined();
+        welcome.addStyleName(ValoTheme.LABEL_H4);
+        welcome.addStyleName(ValoTheme.LABEL_COLORED);
+        labels.addComponent(welcome);
+        /*
+        Label title = new Label("BurguerUCA");
+        title.setSizeUndefined();
+        title.addStyleName(ValoTheme.LABEL_H3);
+        title.addStyleName(ValoTheme.LABEL_LIGHT);
+        labels.addComponent(title);
+        */
+        return labels;
     }
 
     @FunctionalInterface
