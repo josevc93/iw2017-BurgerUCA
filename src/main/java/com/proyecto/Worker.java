552321e9dabf16ec7d7e5d2bb7a6df0a53224e87
josevc93 <josemanuel.vazquezcrespo@gmail.com>
2017-05-25 18:28:30 +0200

    Update Worker.java

--- src/main/java/com/proyecto/Worker.java
+++ src/main/java/com/proyecto/Worker.java
@@ -1,9 +1,11 @@
 package com.proyecto;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity 
