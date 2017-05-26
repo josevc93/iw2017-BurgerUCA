4b374239576637754418ec0d4f325a98c9472123
AlvaroGR92 <gutimero.92@gmail.com>
2017-05-26 10:22:02 +0200

    Act. productrepository

--- src/main/java/com/proyecto/ProductRepository.java
+++ src/main/java/com/proyecto/ProductRepository.java
@@ -3,8 +3,14 @@ package com.proyecto;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.proyecto.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findByNameStartsWithIgnoreCase(String name);
+	
+	@Query("select p from Product p where p.family = :family")
+	List<Product> findByFamily(@Param("family") String family);
 }
